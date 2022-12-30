acc1901478275c04cd68529ae27bcff2c364ebe2
ahmadriski23 <ahmadriskixrpl1csr@gmail.com>
2022-12-30 10:18:39 +0700

    semua dirubah

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ini Github Kek Taiii',
+      title: 'Ini Github Kek Taiii Banget',
       theme: ThemeData(
         // This is the theme of your application.
         //
